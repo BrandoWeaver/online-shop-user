5d0065dfa9c9b842ea4107874762bc4453df8aea
BrandoWeaver <srunrotha614@gmail.com>
2024-10-21 19:27:09 +0700

    fix-login

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -2,11 +2,17 @@ import { Box, IconButton, Toolbar, Typography, useTheme } from "@mui/material";
 import React from "react";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import CheckoutContent from "./CheckoutContent";
+import { useAuthContext } from "../../contexts/AuthContext";
+import { Navigate } from "react-router-dom";
+import { ROUTE_PATH } from "../../utils/route-util";
 
 export const defaultCoord = { lat: 11.5752041, lng: 104.9000041 };
 function Checkout() {
+  const { authState } = useAuthContext();
   const theme = useTheme();
-
+  if (!authState?.isLogIn) {
+    return <Navigate to={ROUTE_PATH.login} replace />;
+  }
   return (
     <Box>
       <Toolbar
